c4618b762620eb4a8b10521e40f060bc5ea6a355
AungKo2019 <highflier.ako1991@gmail.com>
2023-05-12 01:27:24 +0630

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,Button,TextInput,TouchableOpacity} from 'react-native';
+import * as Speech from 'expo-speech'
+import { useState } from 'react';
 
 export default function App() {
+
+  const [thingsToSay,setThingsToSay]=useState("My name is Aung Ko . I am react native Developer");
+
+  // const thingsToSay="My name is Aung Ko . I am react native Developer";
+
+  const speak=()=>{
+    Speech.speak(thingsToSay)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <Text style={styles.text}>{thingsToSay}</Text> */}
+      <TextInput placeholder='Type To Speak' multiline onChangeText={setThingsToSay} value={thingsToSay} style={{width:'90%',height:'20%',borderWidth:1,backgroundColor:'orange'}}/>
+      <TouchableOpacity style={{marginTop:20,width:150,height:50,backgroundColor:'green',borderRadius:20,justifyContent:'center',alignItems:'center'}} onPress={speak}>
+        <Text>Voice</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,4 +31,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    color:'black',
+    marginTop:10,
+    padding:10
+  }
 });
